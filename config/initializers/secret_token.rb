34b75d055a1a0d99dbab4457f6c8f786a8ff1d7f
Lucas Caton <lucascaton@gmail.com>
2013-02-14 11:14:32 -0200

    Initial Rails directory structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyPodcasts::Application.config.secret_token = '3fb28fc8ad1b5a6149308f91637f0ebb650a2524b3a74d8544ad9a7b87acd7ee66c7ae398399fd32dc997e3a70dbd8ddb885814f4872fa0d2433a7dec291a013'

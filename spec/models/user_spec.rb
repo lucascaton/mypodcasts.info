50a70d98db0288361454a5c5bb1127b2450ca01a
Lucas Caton <lucascaton@gmail.com>
2013-02-14 23:43:38 -0200

    Adjust in spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  provider   :string(255)
+#  uid        :string(255)
+#  name       :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'spec_helper'
 
 describe User do

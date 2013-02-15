f7f4d5aed8c3114a7da0a37ceb5041ce355699c9
Lucas Caton <lucascaton@gmail.com>
2013-02-15 00:21:57 -0200

    Improving views

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,11 +2,11 @@ class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(env['omniauth.auth'])
     session[:user_id] = user.id
-    redirect_to root_url, notice: 'Signed in.'
+    redirect_to root_url, notice: 'Logado com sucesso.'
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_url, notice: 'Signed out.'
+    redirect_to root_url
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ MyPodcasts::Application.routes.draw do
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
   get 'signout', to: 'sessions#destroy', as: 'signout'
+
+  resources :users, only: [:show]
+  resources :podcasts, only: [:show, :new, :create]
+  resources :subscriptions, only: [:create]
 end

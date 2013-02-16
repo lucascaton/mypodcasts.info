4daaa5f9b3f242ba07e6e62f56050d99a354d5e6
Lucas Caton <lucascaton@gmail.com>
2013-02-16 13:49:32 -0200

    User#show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id]).decorate
+    @subscriptions = @user.subscriptions
   end
 end
--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -2,6 +2,11 @@
 class UserDecorator < Draper::Decorator
   delegate_all
 
+  # Values for size:
+  # bigger - 73px by 73px
+  # normal - 48px by 48px
+  # mini - 24px by 24px
+  # original - This will be the size the image was originally uploaded in.
   def twitter_photo(size='mini')
     h.image_tag "https://api.twitter.com/1/users/profile_image?screen_name=#{model.name}&size=#{size}"
   end

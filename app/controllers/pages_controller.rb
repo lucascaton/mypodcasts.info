ba5fb96bb72328fcb82de6bb2ef103eff1fb92e8
Lucas Caton <lucascaton@gmail.com>
2013-02-16 01:11:17 -0200

    Adjust in views

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
   def index
+    @podcasts = Podcast.actives
+    @users = current_user ? (User.all - [current_user.model]).map(&:decorate) : User.decorate
   end
 end

ff47f687b12d96776e50ae0b08bbb71d5bbb8a93
Lucas Caton <lucascaton@gmail.com>
2013-02-17 14:32:14 -0300

    Random users in initial page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def index
     @podcasts = Podcast.actives.ordered
-    @users = current_user ? (User.all - [current_user.model]).map(&:decorate) : User.decorate
+    @users = current_user ? (User.order('RANDOM()') - [current_user.model]).map(&:decorate) : User.decorate
   end
 end

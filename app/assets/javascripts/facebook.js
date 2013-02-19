5860c71d30bc233a1f607f3b715bb9b374561438
Lucas Caton <lucascaton@gmail.com>
2013-02-18 23:33:53 -0300

    Including twitter and facebook buttons

---
+++ app/assets/javascripts/facebook.js
@@ -0,0 +1,8 @@
+// Like button
+(function(d, s, id){
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = '//connect.facebook.net/en_US/all.js#xfbml=1&appId=433201703410623';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));

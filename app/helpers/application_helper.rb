6bc3aa1b3eb01445dece6cb78483d1feba5d7f48
Marcio Trindade <marciotrindade@gmail.com>
2013-06-03 23:54:08 -0300

    including helpers to page title and description

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+
+  def title(title)
+    @title = title
+  end
+
+  def show_title
+    title  = @title ? "#{@title} - " : ''
+    title += 'My Podcasts'
+    content_tag :title, title
+  end
+
+  def description(text)
+    @description = text
+  end
+
+  def show_description
+    tag('meta', name: 'description', content: @description) if @description
+  end
 end

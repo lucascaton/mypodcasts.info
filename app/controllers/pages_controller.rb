d7ff9cf614742a2f571dd06518739831095be890
Lucas Caton <lucascaton@gmail.com>
2013-06-24 18:39:25 -0300

    Including flash message

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,9 @@
 class PagesController < ApplicationController
   def index
+    if current_user.nil? || current_user.image_url.nil?
+      flash.now[:notice] = 'O Twitter atualizou sua API e será necessário se autenticar novamente para exibir a imagem do seu usuário.'
+    end
+
     @podcasts = Podcast.actives.ordered
     @users = current_user ? (User.order('RANDOM()') - [current_user.model]).map(&:decorate) : User.decorate
   end
